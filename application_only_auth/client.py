e1dfa113f1088aa0d759c5c8116c6d7436de8102
Pablo SEMINARIO <pabluk@gmail.com>
2013-08-20 11:41:01 +0200

    Changes to be compatible with Python 3.3

--- application_only_auth/client.py
+++ application_only_auth/client.py
@@ -1,6 +1,11 @@
+import sys
 import base64
-import urllib2
 import json
+
+try:
+    import urllib2
+except ImportError:
+    import urllib.request, urllib.error, urllib.parse
 
 
 API_ENDPOINT = 'https://api.twitter.com'
@@ -26,28 +31,47 @@ class Client(object):
         if not self.access_token:
             self.access_token = self._get_access_token()
 
-        request = urllib2.Request(url)
+        if sys.version_info.major < 3:
+            request = urllib2.Request(url)
+        else:
+            request = urllib.request.Request(url)
+
         request.add_header('Authorization', 'Bearer %s' % self.access_token)
-        try:
-            response = urllib2.urlopen(request)
-        except urllib2.HTTPError:
-            raise ClientException
+        if sys.version_info.major < 3:
+            try:
+                response = urllib2.urlopen(request)
+            except urllib2.HTTPError:
+                raise ClientException
+        else:
+            try:
+                response = urllib.request.urlopen(request)
+            except urllib.error.HTTPError:
+                raise ClientException
 
         return response.read()
 
     def rate_limit_status(self):
         """Returns a dict of rate limits by resource."""
         json_response = self.request(REQUEST_RATE_LIMIT)
-        return json.loads(json_response)
+        return json.loads(json_response.decode('utf-8'))
 
     def _get_access_token(self):
         """Obtain a bearer token."""
-        encoded_bearer_token = base64.b64encode('%s:%s' % (self.consumer_key, self.consumer_secret))
-        request = urllib2.Request(REQUEST_TOKEN_URL)
+        bearer_token = '%s:%s' % (self.consumer_key, self.consumer_secret)
+        encoded_bearer_token = base64.b64encode(bearer_token.encode('ascii'))
+        if sys.version_info.major < 3:
+            request = urllib2.Request(REQUEST_TOKEN_URL)
+        else:
+            request = urllib.request.Request(REQUEST_TOKEN_URL)
         request.add_header('Content-Type', 'application/x-www-form-urlencoded;charset=UTF-8')
-        request.add_header('Authorization', 'Basic %s' % encoded_bearer_token)
-        request.add_data('grant_type=client_credentials')
+        request.add_header('Authorization', 'Basic %s' % encoded_bearer_token.decode('utf-8'))
+        request.add_data('grant_type=client_credentials'.encode('ascii'))
 
-        response = urllib2.urlopen(request)
-        data = json.load(response)
+        if sys.version_info.major < 3:
+            response = urllib2.urlopen(request)
+        else:
+            response = urllib.request.urlopen(request)
+
+        raw_data = response.read().decode('utf-8')
+        data = json.loads(raw_data)
         return data['access_token']
