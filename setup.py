1060f73ac0dedffa583c9e9e04db931674d68a7e
Pablo SEMINARIO <pabluk@gmail.com>
2013-10-24 22:27:55 +0200

    Replaced setuptools by distutils into setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,27 @@
-from setuptools import setup, find_packages
-import sys, os
+from distutils.core import setup
 
-version = '0.1'
+
+with open('README.md') as file:
+    long_description = file.read()
+
 
 setup(name='twiiter-application-only-auth',
-      version=version,
-      description="Simple twitter API client to use Application Only Auth",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
-      author='',
-      author_email='',
-      url='',
-      license='',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[],
-      entry_points="""
-# -*- Entry points: -*-
-""",
-      )
+      version='0.3.0',
+      author='Pablo SEMINARIO',
+      author_email='pablo@seminar.io',
+      url='https://github.com/pabluk/twitter-application-only-auth',
+      description='A simple implementation of '
+      'the Twitter Application-only authentication',
+      long_description=long_description,
+      license='GNU General Public License v3 (GPLv3)',
+      packages=['application_only_auth'],
+      provides=['application_only_auth (0.3.0)'],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2.6',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Topic :: Software Development :: Libraries'])
