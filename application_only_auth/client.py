7fafbb0ab9bc2546d649f7165c981df89528534b
Pablo Seminario <pablo@seminar.io>
2022-12-04 22:28:12 +0100

    Reformat files using Python's black

--- application_only_auth/client.py
+++ application_only_auth/client.py
@@ -12,11 +12,10 @@ except ImportError:
     from urllib2 import urlopen, Request, HTTPError
 
 
-API_ENDPOINT = 'https://api.twitter.com'
-API_VERSION = '1.1'
-REQUEST_TOKEN_URL = '%s/oauth2/token' % API_ENDPOINT
-REQUEST_RATE_LIMIT = '%s/%s/application/rate_limit_status.json' % \
-                     (API_ENDPOINT, API_VERSION)
+API_ENDPOINT = "https://api.twitter.com"
+API_VERSION = "1.1"
+REQUEST_TOKEN_URL = "%s/oauth2/token" % API_ENDPOINT
+REQUEST_RATE_LIMIT = "%s/%s/application/rate_limit_status.json" % (API_ENDPOINT, API_VERSION)
 
 
 class ClientException(Exception):
@@ -29,7 +28,7 @@ class Client(object):
     def __init__(self, consumer_key, consumer_secret):
         self.consumer_key = consumer_key
         self.consumer_secret = consumer_secret
-        self.access_token = ''
+        self.access_token = ""
 
     def request(self, url):
         """Send an authenticated request to the Twitter API."""
@@ -37,41 +36,39 @@ class Client(object):
             self.access_token = self._get_access_token()
 
         request = Request(url)
-        request.add_header('Authorization', 'Bearer %s' % self.access_token)
+        request.add_header("Authorization", "Bearer %s" % self.access_token)
         try:
             response = urlopen(request)
         except HTTPError:
             raise ClientException
 
-        raw_data = response.read().decode('utf-8')
+        raw_data = response.read().decode("utf-8")
         data = json.loads(raw_data)
         return data
 
-    def rate_limit_status(self, resource=''):
+    def rate_limit_status(self, resource=""):
         """Returns a dict of rate limits by resource."""
         response = self.request(REQUEST_RATE_LIMIT)
         if resource:
-            resource_family = resource.split('/')[1]
-            return response['resources'][resource_family][resource]
+            resource_family = resource.split("/")[1]
+            return response["resources"][resource_family][resource]
         return response
 
     def _get_access_token(self):
         """Obtain a bearer token."""
-        bearer_token = '%s:%s' % (self.consumer_key, self.consumer_secret)
-        encoded_bearer_token = base64.b64encode(bearer_token.encode('ascii'))
+        bearer_token = "%s:%s" % (self.consumer_key, self.consumer_secret)
+        encoded_bearer_token = base64.b64encode(bearer_token.encode("ascii"))
         request = Request(REQUEST_TOKEN_URL)
-        request.add_header('Content-Type',
-                           'application/x-www-form-urlencoded;charset=UTF-8')
-        request.add_header('Authorization',
-                           'Basic %s' % encoded_bearer_token.decode('utf-8'))
+        request.add_header("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
+        request.add_header("Authorization", "Basic %s" % encoded_bearer_token.decode("utf-8"))
 
-        request_data = 'grant_type=client_credentials'.encode('ascii')
-        if sys.version_info < (3,4):
+        request_data = "grant_type=client_credentials".encode("ascii")
+        if sys.version_info < (3, 4):
             request.add_data(request_data)
         else:
             request.data = request_data
 
         response = urlopen(request)
-        raw_data = response.read().decode('utf-8')
+        raw_data = response.read().decode("utf-8")
         data = json.loads(raw_data)
-        return data['access_token']
+        return data["access_token"]
--- tests/test_client.py
+++ tests/test_client.py
@@ -26,23 +26,21 @@ def fake_urlopen(request):
     """
     # Map path from url to a file
     parsed_url = urlparse(request.get_full_url())
-    resource_file = os.path.normpath('tests/resources%s' % parsed_url.path)
+    resource_file = os.path.normpath("tests/resources%s" % parsed_url.path)
 
     try:
-        return open(resource_file, mode='rb')
+        return open(resource_file, mode="rb")
     except IOError:
-        raise HTTPError(request.get_full_url, 404,
-                        "HTTP Error 404: Not Found", {}, None)
+        raise HTTPError(request.get_full_url, 404, "HTTP Error 404: Not Found", {}, None)
 
 
 class ClientTestCase(unittest.TestCase):
     """Test case for the client methods."""
 
     def setUp(self):
-        self.patcher = patch('application_only_auth.client.urlopen',
-                             fake_urlopen)
+        self.patcher = patch("application_only_auth.client.urlopen", fake_urlopen)
         self.patcher.start()
-        self.client = Client('xxxxxx', 'xxxxxx')
+        self.client = Client("xxxxxx", "xxxxxx")
 
     def tearDown(self):
         self.patcher.stop()
@@ -50,24 +48,23 @@ class ClientTestCase(unittest.TestCase):
     def test_rate_limit(self):
         """Test rate limit response."""
         status = self.client.rate_limit_status()
-        resource_status = status['resources']['search']['/search/tweets']
-        expected_status = {'remaining': 450, 'limit': 450, 'reset': 1380131036}
+        resource_status = status["resources"]["search"]["/search/tweets"]
+        expected_status = {"remaining": 450, "limit": 450, "reset": 1380131036}
         self.assertEqual(resource_status, expected_status)
 
     def test_rate_limit_with_resource(self):
         """Test rate limit response using a resource as parameter."""
-        response = self.client.rate_limit_status('/search/tweets')
-        expected = {'remaining': 450, 'limit': 450, 'reset': 1380131036}
+        response = self.client.rate_limit_status("/search/tweets")
+        expected = {"remaining": 450, "limit": 450, "reset": 1380131036}
         self.assertEqual(response, expected)
 
     def test_show_status(self):
         """Test status show response."""
-        resource_url = 'https://api.twitter.com/1.1' \
-                       '/statuses/show.json?id=316683059296624640'
+        resource_url = "https://api.twitter.com/1.1" "/statuses/show.json?id=316683059296624640"
         tweet = self.client.request(resource_url)
-        self.assertEqual(tweet['id_str'], "316683059296624640")
+        self.assertEqual(tweet["id_str"], "316683059296624640")
 
     def test_invalid_resource(self):
         """Test status show response."""
-        resource_url = 'https://api.twitter.com/1.1/resource/invalid.json'
+        resource_url = "https://api.twitter.com/1.1/resource/invalid.json"
         self.assertRaises(ClientException, self.client.request, resource_url)
